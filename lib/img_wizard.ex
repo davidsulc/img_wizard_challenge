fb79690264fb20eea014a79085c538a5db33d915
David Sulc <davidsulc@gmail.com>
2021-07-07 19:11:33 +0200

    initial import

---
+++ lib/img_wizard.ex
@@ -0,0 +1,18 @@
+defmodule ImgWizard do
+  @moduledoc """
+  Documentation for `ImgWizard`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ImgWizard.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/img_wizard_test.exs
@@ -0,0 +1,8 @@
+defmodule ImgWizardTest do
+  use ExUnit.Case
+  doctest ImgWizard
+
+  test "greets the world" do
+    assert ImgWizard.hello() == :world
+  end
+end
