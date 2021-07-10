82338b059164b18ca20fbd6638069e771aef560d
David Sulc <davidsulc@gmail.com>
2021-07-10 10:35:12 +0200

    implement PUT /info

--- lib/img_wizard_api/endpoint.ex
+++ lib/img_wizard_api/endpoint.ex
@@ -1,14 +1,48 @@
 defmodule ImgWizardApi.Endpoint do
   use Plug.Router
 
+  alias ImgWizard.MetaInfo
+
   plug(:match)
+  plug(Plug.Parsers, parsers: [:multipart])
   plug(:dispatch)
 
-  get "/hello" do
-    send_resp(conn, 200, "world")
+  put "/info" do
+    path = get_image_path(conn)
+
+    case ImgWizard.metadata(path) do
+      {:ok, %MetaInfo{} = info} ->
+        result =
+          info
+          |> Map.from_struct()
+          |> Jason.encode!()
+
+        send_resp(conn, 200, result)
+
+      {:error, %type{} = error} ->
+        result =
+          error
+          |> Map.from_struct()
+          |> Map.delete(:__exception__)
+          |> Map.put(:error, format_error_name(type))
+          |> Jason.encode!()
+
+        send_resp(conn, status(type), result)
+    end
   end
 
   match _ do
     send_resp(conn, 404, "Not implemented")
+  end
+
+  defp get_image_path(%Plug.Conn{body_params: %{"image" => %{path: path}}}), do: path
+
+  defp status(ImgWizard.UnhandledFileTypeError), do: 422
+  defp status(_), do: 500
+
+  defp format_error_name(error_module) when is_atom(error_module) do
+    error_module
+    |> Atom.to_string()
+    |> String.replace(~r/^Elixir\.ImgWizard\./, "")
   end
 end
--- mix.exs
+++ mix.exs
@@ -32,7 +32,8 @@ defmodule ImgWizard.MixProject do
       {:dialyxir, "~> 1.1", only: [:dev], runtime: false},
       {:ex_doc, "~> 0.24", only: :dev, runtime: false},
       {:ex_image_info, "~> 0.2.4"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:jason, "~> 1.2"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
