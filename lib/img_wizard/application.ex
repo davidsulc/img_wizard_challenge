6bf4ddd1083de51aec1cda2e04b0005fda3f77ed
David Sulc <davidsulc@gmail.com>
2021-07-10 08:20:19 +0200

    add PlugCowboy to handle api requests
    This originally started as a standalone image transformation lib, but given the scope of the assignment,
    it doesn't really make that much sense to have the API as a completely separate mix project. So I decided
    to add the API portion in this project as well...

---
+++ lib/img_wizard/application.ex
@@ -0,0 +1,18 @@
+defmodule ImgWizard.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    children = [
+      {Plug.Cowboy, scheme: :http, plug: ImgWizardApi.Endpoint, options: [port: 4040]}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: ImgWizard.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
--- mix.exs
+++ mix.exs
@@ -14,7 +14,8 @@ defmodule ImgWizard.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger],
+      mod: {ImgWizard.Application, []}
     ]
   end
 
@@ -23,7 +24,8 @@ defmodule ImgWizard.MixProject do
     [
       {:dialyxir, "~> 1.1", only: [:dev], runtime: false},
       {:ex_doc, "~> 0.24", only: :dev, runtime: false},
-      {:ex_image_info, "~> 0.2.4"}
+      {:ex_image_info, "~> 0.2.4"},
+      {:plug_cowboy, "~> 2.5"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
