04ec4a00c257ecb4c88825f30e8f1a387b8ab123
David Sulc <davidsulc@gmail.com>
2021-07-11 16:10:24 +0200

    use different default port for testing
    This makes it more convenient to run tests when the app is already running.

--- config/runtime.exs
+++ config/runtime.exs
@@ -1,9 +1,16 @@
 import Config
 
+default_port = fn ->
+  case config_env() do
+    :test -> 4041
+    _ -> 4040
+  end
+end
+
 port =
   case System.fetch_env("IMG_WIZARD_PORT") do
     {:ok, port} -> String.to_integer(port)
-    :error -> 4040
+    :error -> default_port.()
   end
 
 config :img_wizard, :port, port
